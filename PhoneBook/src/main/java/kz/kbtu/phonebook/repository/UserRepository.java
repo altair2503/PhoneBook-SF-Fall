ff09ed5c2ef5c3fe308858adfbbffd713939a767
Altair <altair200294@gmail.com>
2023-12-04 14:43:53 +0600

    finished jwt authorization

--- PhoneBook/src/main/java/kz/kbtu/phonebook/repository/UserRepository.java
+++ PhoneBook/src/main/java/kz/kbtu/phonebook/repository/UserRepository.java
@@ -4,7 +4,11 @@ import kz.kbtu.phonebook.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Optional<User> findByEmail(String email);
+    Optional<User> findByFirstName(String fistName);
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
---
+++ PhoneBook/src/test/java/kz/kbtu/phonebook/user/UserRepositoryTests.java
@@ -0,0 +1,30 @@
+package kz.kbtu.phonebook.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kz.kbtu.phonebook.domain.User;
+import kz.kbtu.phonebook.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User newUser = new User("mana@kbtu.kz", "manasila", "Mana", "Esim", "879743512343");
+        User savedUser = repo.save(newUser);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
