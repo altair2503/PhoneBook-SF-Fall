4d17ac5599d76536f597808fa969d7d16af32131
PP2-21B030774 <azat.amenov@gmail.com>
2023-12-04 14:48:11 +0600

    Second day 2.0

--- PhoneBook/src/main/java/kz/kbtu/phonebook/repository/UserRepository.java
+++ PhoneBook/src/main/java/kz/kbtu/phonebook/repository/UserRepository.java
@@ -4,6 +4,8 @@ import kz.kbtu.phonebook.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
